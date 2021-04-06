065dff47d20eaf793c5f4303f987cdbdbc67acfb
Isaiah <anyiminene@gmail.com>
2021-04-05 18:56:11 -0700

    balloonski models in. new gatorhead still needed

--- src/scenes/Gotham/Balloonski/index.tsx
+++ src/scenes/Gotham/Balloonski/index.tsx
@@ -1,6 +1,7 @@
 import Gatorhead from "./models/Gatorhead";
-import Ghost from "./models/Ghost";
-import Moneycat from "./models/Moneycat";
+import Ghost from "./models/Ghost1";
+import Moneycat from "./models/Moneycat1";
+import { Floating } from "spacesvr";
 
 const Balloonski = () => {
   return (
@@ -8,7 +9,7 @@ const Balloonski = () => {
       <Moneycat
         position={[-4.55, 0, 11.2]}
         rotation-y={2.34}
-        scale={[4.5, 4.5, 4.5]}
+        scale={[7, 7, 7]}
         name="cat"
       />
       <Gatorhead
@@ -17,7 +18,16 @@ const Balloonski = () => {
         scale={[5, 5, 5]}
         name="gator"
       />
-      {/*<Ghost />*/}
+      <Floating height={0.15} speed={3}>
+        <Ghost
+          name="ghost"
+          // position={[-4.49, 1.28, -3.26]}
+          // rotation={[-0.7, 0.67, 0.5]}
+          position={[-4.5, 1.5, -3.3]}
+          rotation-y={0.67}
+          scale={[10, 10, 10]}
+        />
+      </Floating>
     </group>
   );
 };
